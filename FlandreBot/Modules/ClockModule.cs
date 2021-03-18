f5cca5175b2fe0787025bfc9813b056969a8f776
eliphat.f.s <shiruoxi2010@163.com>
2021-03-18 22:01:40 +0800

    Bug Fix: Unstoppable Clock

--- FlandreBot/Modules/ClockModule.cs
+++ FlandreBot/Modules/ClockModule.cs
@@ -17,12 +17,14 @@ namespace Module {
             var old = DateTime.Now;
             while (true) {
                 var cur = DateTime.Now;
-                if (old.Hour != cur.Hour)
+                if (old.Hour != cur.Hour) {
+                    old = cur;
                     foreach (var connector in connectors)
                         foreach (var gid in config.groupsClock)
                             await connector.SendMessage(new GroupTarget { id = gid }, new Message {
                                 new LocalImageSub { resourcePath = $"clock/{cur.Hour % 12}.jpg" }
                             });
+                }
                 await Task.Delay(1000);
             }
         }
