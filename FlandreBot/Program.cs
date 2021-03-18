90728ef4e438f91c4008f8c034c88a8cceaa8011
eliphat.f.s <shiruoxi2010@163.com>
2021-03-18 20:16:24 +0800

    New Project

---
+++ FlandreBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace FlandreBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
